97a61a3b6af77a4bf1920bf00f8035c20fc09acc
Adalex3 <126013491+adalex3-limbitless@users.noreply.github.com>
2023-09-16 00:27:16 -0400

    Secrets file 🤫
    🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫

--- Flutter/myapp/lib/api/chat_api.dart
+++ Flutter/myapp/lib/api/chat_api.dart
@@ -1,12 +1,13 @@
 import 'package:myapp/models/chat_message.dart';
 import 'package:dart_openai/openai.dart';
+import 'package:myapp/api/secrets.dart';
 
 class ChatApi {
   static const _model = 'gpt-3.5-turbo';
 
   ChatApi() {
-    OpenAI.apiKey = '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';
-    OpenAI.organization = 'Hynds Productions, LLC';
+    OpenAI.apiKey = api_key;
+    OpenAI.organization = api_org;
   }
 
   Future<String> completeChat(List<ChatMessage> messages) async {
---
+++ Flutter/myapp/lib/api/secrets.dart
@@ -0,0 +1,2 @@
+const api_key = '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';
+const api_org = 'Hynds Productions, LLC';
