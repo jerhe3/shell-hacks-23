a499d1150526cae74124b2b356c6651f8ac59c5e
Adalex3 <126013491+adalex3-limbitless@users.noreply.github.com>
2023-09-16 00:30:02 -0400

    FIXED secrets??

--- Flutter/myapp/lib/api/secrets.dart
+++ Flutter/myapp/lib/api/secrets.dart
@@ -1,2 +1,2 @@
-const api_key = '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';
+const api_key = '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';
 const api_org = 'Hynds Productions, LLC';
