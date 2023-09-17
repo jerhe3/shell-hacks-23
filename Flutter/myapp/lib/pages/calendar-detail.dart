b01858e475e0d69d0c22d7da1c7134962c3a33a5
Adalex3 <126013491+adalex3-limbitless@users.noreply.github.com>
2023-09-17 07:43:24 -0400

    trying another calendar lirbary

--- Flutter/myapp/lib/pages/calendar-detail.dart
+++ Flutter/myapp/lib/pages/calendar-detail.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:calendar_view';
+import 'package:myapp/ui/calendar.dart';
 
 class CalendarDetail extends StatefulWidget {
   @override
@@ -11,7 +11,7 @@ class CalendarDetailState extends State<CalendarDetail> {
   @override
   Widget build(BuildContext context) {
 
-    // return 
+    
 
   }
 
